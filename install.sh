741f6c8b89c6b326f9ed9c5818d0022f4051c389
Scott <weston.scot@gmail.com>
2016-08-24 09:43:16 +0200

    Added tmux conf and zshrc to install script

--- install.sh
+++ install.sh
@@ -5,6 +5,8 @@ mkdir ./backup
 mkdir ./bundle
 
 ln -sf ~/.vim/.vimrc ~/.vimrc
+ln -sf ~/.vim/.tmux.conf ~/.tmux.conf
+ln -sf ~/.vim/.zshrc ~/.zshrc
 
 git clone https://github.com/vundlevim/vundle.vim.git ./bundle/Vundle.vim
 vim +PluginInstall +qall
