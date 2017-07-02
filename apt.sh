292c18ac1d3b5b0b292c2a39e6f6a87da5169898
Scott <weston.scot@gmail.com>
2017-07-02 10:13:34 +0200

    Add curl to the packages to install

--- apt.sh
+++ apt.sh
@@ -12,6 +12,7 @@ sudo apt-get install -y python3-dev
 sudo apt-get install -y python-dev
 sudo apt-get install -y python3-pip
 sudo apt-get install -y python-pip
+sudo apt-get install -y curl
 
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
 echo "deb http://download.mono-project.com/repo/debian wheezy main" | sudo tee /etc/apt/sources.list.d/mono-xamarin.list
