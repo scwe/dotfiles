d56875686bb07a6b374fb5b13cf5758702b56d4d
scwe <scottw@plexapp.com>
2026-05-01 17:14:25 +1200

    Modernize apt.sh
    creationix/nvm is the old org name (it redirects but I might as well
    point at the canonical nvm-sh/nvm). Pinning node 20.11 made sense in
    2024; --lts means future-me gets whatever the current LTS is on
    reinstall. Drop the dead commented apt-nodejs lines. And -y on the
    final apt-get upgrade so the script doesn't hang waiting on a Y/n.

--- apt.sh
+++ apt.sh
@@ -1,21 +1,16 @@
 #!/bin/bash
 sudo apt install -y gitk vim vim-gtk3 tmux zsh curl gnome-terminal cmake
 
-# Install nodejs
-#curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
-#sudo apt-get install -y nodejs
-
-
 # Install nvm
-curl -o- https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
 
-nvm install 20.11
+nvm install --lts
 npm install --global yarn
 
 # Install uv (Python package + project manager, also handles Python versions)
 curl -LsSf https://astral.sh/uv/install.sh | sh
 
-sudo apt-get upgrade
+sudo apt-get upgrade -y
 
 # Oh my zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
